8123fbb43a00b71581177ef85f368dec59298551
Matej Hrica <mhrica@redhat.com>
2023-09-05 17:42:52 +0200

    use edge-triggered IN OUT epoll for passt_socket + code style cleanup

--- src/devices/src/virtio/net/event_handler.rs
+++ src/devices/src/virtio/net/event_handler.rs
@@ -3,7 +3,7 @@
 
 use std::os::unix::io::AsRawFd;
 
-use polly::event_manager::{EventManager, Pollable, Subscriber};
+use polly::event_manager::{EventManager, Subscriber};
 use utils::epoll::{EpollEvent, EventSet};
 
 use crate::virtio::net::device::Net;
@@ -12,7 +12,7 @@ use crate::virtio::VirtioDevice;
 
 impl Net {
     fn process_activate_event(&self, event_manager: &mut EventManager) {
-        debug!("net: activate event");
+        log::debug!("net: activate event");
         if let Err(e) = self.activate_evt.read() {
             log::error!("Failed to consume net activate event: {:?}", e);
         }
@@ -41,33 +41,6 @@ impl Net {
             log::error!("Failed to unregister net activate evt: {:?}", e);
         });
     }
-
-    fn enable_past_out_event_if_necessary(&mut self, evmgr: &mut EventManager) {
-        if self.passt_has_unfinished_write() {
-            evmgr
-                .modify(
-                    self.raw_passt_socket_fd() as Pollable,
-                    EpollEvent::new(
-                        EventSet::IN | EventSet::OUT,
-                        self.raw_passt_socket_fd() as u64,
-                    ),
-                )
-                .unwrap();
-            log::trace!("Enabled OUT listener on passt sock fd");
-        };
-    }
-
-    fn disable_past_out_event_if_unnecessary(&mut self, evmgr: &mut EventManager) {
-        if !self.passt_has_unfinished_write() {
-            evmgr
-                .modify(
-                    self.raw_passt_socket_fd() as Pollable,
-                    EpollEvent::new(EventSet::IN, self.raw_passt_socket_fd() as u64),
-                )
-                .unwrap();
-            log::trace!("Disabled OUT listener on passt sock fd");
-        };
-    }
 }
 
 impl Subscriber for Net {
@@ -81,29 +54,28 @@ impl Subscriber for Net {
             let passt_socket = self.raw_passt_socket_fd();
             let activate_fd = self.activate_evt.as_raw_fd();
 
-            if event_set.contains(EventSet::OUT) && source == passt_socket {
-                self.process_passt_socket_writeable();
-                self.disable_past_out_event_if_unnecessary(evmgr);
-            }
-
-            // Looks better than C style if/else if/else.
-            match () {
-                _ if event_set.contains(EventSet::IN) && source == virtq_rx_ev_fd => {
-                    self.process_rx_queue_event()
+            match event_set {
+                EventSet::IN if source == activate_fd => {
+                    self.process_activate_event(evmgr);
                 }
-                _ if event_set.contains(EventSet::IN) && source == passt_socket => {
-                    self.process_passt_rx_event()
+                EventSet::IN if source == virtq_rx_ev_fd => {
+                    self.process_rx_queue_event();
                 }
-                _ if event_set.contains(EventSet::IN) && source == virtq_tx_ev_fd => {
+                EventSet::IN if source == virtq_tx_ev_fd => {
                     self.process_tx_queue_event();
-                    self.enable_past_out_event_if_necessary(evmgr);
                 }
-                _ if event_set.contains(EventSet::IN) && activate_fd == source => {
-                    self.process_activate_event(evmgr)
+                _ if source == passt_socket => {
+                    if event_set.contains(EventSet::IN) {
+                        self.process_passt_rx_event()
+                    }
+
+                    if event_set.contains(EventSet::OUT) {
+                        self.process_passt_socket_writeable()
+                    }
                 }
                 _ => {
                     log::warn!(
-                        "Received unknown event: {:?} from source: {:?}",
+                        "Received unknown event: {:?} from fd: {:?}",
                         event_set,
                         source
                     );
@@ -122,7 +94,10 @@ impl Subscriber for Net {
             vec![
                 EpollEvent::new(EventSet::IN, self.queue_evts[RX_INDEX].as_raw_fd() as u64),
                 EpollEvent::new(EventSet::IN, self.queue_evts[TX_INDEX].as_raw_fd() as u64),
-                EpollEvent::new(EventSet::IN, self.raw_passt_socket_fd() as u64),
+                EpollEvent::new(
+                    EventSet::IN | EventSet::OUT | EventSet::EDGE_TRIGGERED,
+                    self.raw_passt_socket_fd() as u64,
+                ),
             ]
         } else {
             vec![EpollEvent::new(
